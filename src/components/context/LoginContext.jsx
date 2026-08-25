45787b4d050fee255228cdffe8fa6f2c56375e6b
Abirokaya <rokayaabinash9@gmail.com>
2026-08-26 00:37:12 +0545

    adstd

--- src/components/context/LoginContext.jsx
+++ src/components/context/LoginContext.jsx
@@ -3,8 +3,10 @@ import React, { createContext, useState } from "react";
 export const LoginContext = createContext();
 
 export const LoginProvider = ({ children }) => {
-  const [userRole, setRole] = useState("Citizen");
-  const [isLogin, setisLogin] = useState(true);
+  // User is logged out by default
+  const [userRole, setRole] = useState(null);
+  const [isLogin, setisLogin] = useState(false);
+
   return (
     <LoginContext.Provider
       value={{
